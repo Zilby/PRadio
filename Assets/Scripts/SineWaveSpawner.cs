9f9c2d97cdca8ce68fffbb9570c1d7ce0a24f83f
tday5 <trevday@icloud.com>
2018-01-26 21:08:23 -0500

    First working wave bars

---
+++ Assets/Scripts/SineWaveSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineWaveSpawner : MonoBehaviour {
+
+    // This is essentially cached, don't trust it
+    public float amplitude;
+
+    public float frequency;
+
+    public float phase;
+
+    public float spawnFrequency;
+
+    private bool paused = false;
+
+    public float SinFunction(float time) {
+        return amplitude * Mathf.Sin(2 * Mathf.PI * frequency * time + phase);
+    }
+
+    void Start() {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn() {
+        while (!paused) {
+            yield return new WaitForSeconds(spawnFrequency);
+            GameObject bar = ObjectPooler.instance.GetPooledObjectAtPosition("WaveBar", this.transform.position, Quaternion.identity);
+            if (bar != null) {
+                bar.gameObject.SetActive(true);
+                bar.GetComponent<WaveBar>().SetBarSprite(this.SinFunction(Time.time));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WaveBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveBar : MonoBehaviour {
+
+    public List<Sprite> barSprites;
+
+    public float moveSpeed;
+
+    private SpriteRenderer spRenderer;
+
+    public void SetBarSprite(float yVal) {
+        if (this.spRenderer == null) {
+            this.spRenderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+        this.spRenderer.sprite = barSprites[0];
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.transform.Translate(new Vector3(this.moveSpeed * Time.time, 0, 0));
+    }
+}
