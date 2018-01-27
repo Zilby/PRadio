0c9064198430dead4e49509a10e584396e7941d3
Chris Juchem <chrisjuchem15@gmail.com>
2018-01-27 14:41:46 -0500

    blank bars at start

--- Assets/Scripts/SineWaveSpawner.cs
+++ Assets/Scripts/SineWaveSpawner.cs
@@ -18,6 +18,7 @@ public class SineWaveSpawner : MonoBehaviour {
 	private float curX = 0;
     private float baseFrequency = 0.015f;
 	private float vOffset = 1.9f;
+	private float barWidth = 0.4f;
 
     public float SinFunction(float x) {
         if (amplitude > cappedAmplitude) {
@@ -27,26 +28,31 @@ public class SineWaveSpawner : MonoBehaviour {
     }
 
     void Start() {
-        StartCoroutine(Spawn());
+        for (int i = 0; i < numBars; i++) {
+            spawnBar(0, 1, i);
+            spawnBar(0, -1, i);
+        }
+        
+        StartCoroutine(SpawnLoop());
     }
 
-    IEnumerator Spawn() {
+    IEnumerator SpawnLoop() {
         while (!paused) {
             yield return new WaitForSeconds(spawnTime);
 			this.curX += this.baseFrequency;
             float sin = this.SinFunction(this.curX);
-			subSpawn(sin, 1);
-			subSpawn(sin, -1);
+			spawnBar(sin, 1, 0);
+			spawnBar(sin, -1, 0);
         }
     }
 
-	void subSpawn(float sin,  int flip) {
+	void spawnBar(float sin,  int flip, int spawnPos) {
 		if (sin * flip < 0) {
 			sin = 0;  
 		}
-		GameObject bar = ObjectPooler.instance.GetPooledObjectAtPosition("WaveBar", new Vector3(this.transform.position.x, this.transform.position.y+(vOffset * flip), this.transform.position.z), Quaternion.identity);
+        GameObject bar = ObjectPooler.instance.GetPooledObjectAtPosition("WaveBar", new Vector3(this.transform.position.x + (spawnPos * barWidth), this.transform.position.y+(vOffset * flip), this.transform.position.z), Quaternion.identity);
 		if (bar != null) {
-			bar.GetComponent<WaveBar>().Init(numBars, spawnTime).SetBarSprite(sin);
+            bar.GetComponent<WaveBar>().Init(numBars - spawnPos, spawnTime, barWidth).SetBarSprite(sin);
 			bar.gameObject.SetActive(true);
 		}
 	}
