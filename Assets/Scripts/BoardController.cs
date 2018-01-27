a76ff613cb8091a6fbc2be2735ba47cea35bd4fe
tday5 <trevday@icloud.com>
2018-01-27 15:31:06 -0500

    Make the spawner and stuff work together

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -5,130 +5,124 @@ using UnityEngine.UI;
 
 public class BoardController : MonoBehaviour {
 
-	public Slider frequencySlider;
-	public Slider amplitudeSlider;
-	public Slider impedanceSlider;
-	public Slider distanceSlider;
+    public Slider frequencySlider;
+    public Slider amplitudeSlider;
+    public Slider impedanceSlider;
+    public Slider distanceSlider;
+    public SineWaveSpawner waveSpawner;
 
-	/// <summary>
-	/// The current temperature. 
-	/// 	Temperature increases or decreases over time depending on the value of 
-	/// 	(power + (abs(targetImpedance - impedance)) + distance)
-	/// </summary>
-	private float temperature = 0;
+    /// <summary>
+    /// The current temperature. 
+    /// 	Temperature increases or decreases over time depending on the value of 
+    /// 	(power + (abs(targetImpedance - impedance)) + distance)
+    /// </summary>
+    private float temperature = 0;
     private const float MAX_TEMPERATURE = 100f;
-	private const float COOLDOWN_RATE = 0.5f;
-	private const float TEMPERATURE_CHANGE_RATE = 1.0F;
+    private const float COOLDOWN_RATE = 0.5f;
+    private const float TEMPERATURE_CHANGE_RATE = 1.0F;
 
-	/// <summary>
-	/// The current impedance
-	/// </summary>
+    /// <summary>
+    /// The current impedance
+    /// </summary>
     private float impedance;
-	private float targetImpedance;
+    private float targetImpedance;
 
-	/// <summary>
-	/// The current frequency
-	/// </summary>
-	private float frequency;
-	private float targetFrequency;
-	public KeyValuePair<float, float> frequencyRange;
+    /// <summary>
+    /// The current frequency
+    /// </summary>
+    private float frequency;
+    private float targetFrequency;
+    public float maxFrequency;
 
-	/// <summary>
-	/// The current amplitude
-	/// </summary>
+    /// <summary>
+    /// The current amplitude
+    /// </summary>
     private float amplitude;
-	private float targetAmplitude;
-	public KeyValuePair<float, float> amplitudeRange;
+    private float targetAmplitude;
+    public float maxAmplitude;
 
-	private float power;
-	private float distance;
+    private float power;
+    private float distance;
 
-	private float interactionCounter = 0;
-	private const float INTERACTION_TIMER = 2.0f;
-	private const float INTERACTION_POWER_INCREASE = 1.0f;
+    private float interactionCounter = 0;
+    private const float INTERACTION_TIMER = 2.0f;
+    private const float INTERACTION_POWER_INCREASE = 1.0f;
 
-	private bool activated;
-	public bool Activated
-	{
-		set
-		{
-			activated = value;
-			if (activated)
-			{
-				interactionCounter = 0;
-				StartCoroutine(SetTemperature());
-			}
-		}
-	}
+    private bool activated;
+    public bool Activated
+    {
+        set
+        {
+            activated = value;
+            if (activated) {
+                interactionCounter = 0;
+                StartCoroutine(SetTemperature());
+            }
+        }
+    }
 
-	private float Power
-	{
-		get { return power; }
-	}
+    private float Power
+    {
+        get { return power; }
+    }
 
-	public float Distance
-	{
-		get { return distance; }
-	}
+    public float Distance
+    {
+        get { return distance; }
+    }
 
-	public void Start()
-	{
-		StartCoroutine(ReduceInteractionPower());
-	}
+    public void Start() {
+        AssignViewEvents();
+        waveSpawner.amplitude = amplitude;
+        waveSpawner.frequency = frequency;
+        StartCoroutine(ReduceInteractionPower());
+    }
 
-	private void AssignViewEvents()
-	{
-		frequencySlider.onValueChanged.AddListener(ModifyFrequency);
-		amplitudeSlider.onValueChanged.AddListener(ModifyAmplitude);
-		impedanceSlider.onValueChanged.AddListener(ModifyImpedance);
-		distanceSlider.onValueChanged.AddListener(ModifyDistance);
-	}
+    private void AssignViewEvents() {
+        frequencySlider.onValueChanged.AddListener(ModifyFrequency);
+        amplitudeSlider.onValueChanged.AddListener(ModifyAmplitude);
+        impedanceSlider.onValueChanged.AddListener(ModifyImpedance);
+        distanceSlider.onValueChanged.AddListener(ModifyDistance);
+    }
 
-	private IEnumerator SetTemperature()
-	{
-		while(activated)
-		{
-			yield return new WaitForSeconds(TEMPERATURE_CHANGE_RATE);
-			power = distance + (Mathf.Abs(targetImpedance - impedance))  / Mathf.Max(amplitudeRange.Value, frequencyRange.Value)
-				+ (interactionCounter > 0 ? INTERACTION_POWER_INCREASE : 0);
-			temperature += power - 0.5f;
-			temperature = Mathf.Max(0, temperature);
-		}
-	}
+    private IEnumerator SetTemperature() {
+        while (activated) {
+            yield return new WaitForSeconds(TEMPERATURE_CHANGE_RATE);
+            power = distance + (Mathf.Abs(targetImpedance - impedance)) / Mathf.Max(maxAmplitude, maxFrequency)
+                + (interactionCounter > 0 ? INTERACTION_POWER_INCREASE : 0);
+            temperature += power - 0.5f;
+            temperature = Mathf.Max(0, temperature);
+        }
+    }
 
-	private IEnumerator ReduceInteractionPower()
-	{
-		while(activated)
-		{
-			if (interactionCounter > 0)
-			{
-				float increment = 0.2f;
-				yield return new WaitForSeconds(increment);
-				interactionCounter -= increment;
-			}
-			yield return null;
-		}
-	}
+    private IEnumerator ReduceInteractionPower() {
+        while (activated) {
+            if (interactionCounter > 0) {
+                float increment = 0.2f;
+                yield return new WaitForSeconds(increment);
+                interactionCounter -= increment;
+            }
+            yield return null;
+        }
+    }
 
-	public void RandomizeValues() {
-		RandomizeFrequency();
-		RandomizeAmplitude();
+    public void RandomizeValues() {
+        RandomizeFrequency();
+        RandomizeAmplitude();
         this.RandomizeImpedance();
     }
 
-	public void RandomizeFrequency()
-	{
-		this.targetFrequency = Random.Range(frequencyRange.Key, frequencyRange.Value);
-	}
+    public void RandomizeFrequency() {
+        this.targetFrequency = Random.Range(0.0f, maxFrequency);
+    }
 
-	public void RandomizeAmplitude()
-	{
-		this.targetAmplitude = Random.Range(amplitudeRange.Key, amplitudeRange.Value);
-	}
+    public void RandomizeAmplitude() {
+        this.targetAmplitude = Random.Range(0.0f, maxAmplitude);
+    }
 
-	public void RandomizeImpedance() {
-        this.targetImpedance = Random.Range(Mathf.Min(this.targetFrequency, this.targetAmplitude), 
-			Mathf.Max(this.targetFrequency, this.targetAmplitude));
+    public void RandomizeImpedance() {
+        this.targetImpedance = Random.Range(Mathf.Min(this.targetFrequency, this.targetAmplitude),
+            Mathf.Max(this.targetFrequency, this.targetAmplitude));
     }
 
     void Awake() {
@@ -136,25 +130,27 @@ public class BoardController : MonoBehaviour {
     }
 
     public void ModifyFrequency(float amount) {
-        this.frequency = amount;
-		interactionCounter = INTERACTION_TIMER;
+        this.frequency = amount * maxFrequency;
+        interactionCounter = INTERACTION_TIMER;
         this.RandomizeImpedance();
+        waveSpawner.frequency = frequency;
     }
 
     public void ModifyAmplitude(float amount) {
-        this.amplitude = amount;
-		interactionCounter = INTERACTION_TIMER;
+        this.amplitude = amount * maxAmplitude;
+        interactionCounter = INTERACTION_TIMER;
         this.RandomizeImpedance();
+        waveSpawner.amplitude = amplitude;
     }
 
     public void ModifyDistance(float amount) {
-		interactionCounter = INTERACTION_TIMER;
-		this.distance = amount;
+        interactionCounter = INTERACTION_TIMER;
+        this.distance = amount;
     }
 
     public void ModifyImpedance(float amount) {
-		interactionCounter = INTERACTION_TIMER;
-		this.impedance = amount;
+        interactionCounter = INTERACTION_TIMER;
+        this.impedance = amount;
     }
 
     public float PercentageCorrect() {
--- Assets/Scripts/SineWaveSpawner.cs
+++ Assets/Scripts/SineWaveSpawner.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class SineWaveSpawner : MonoBehaviour {
 
     public float cappedAmplitude;
-	public int numBars;
+    public int numBars;
 
     // This is essentially cached, don't trust it
+    [System.NonSerialized]
     public float amplitude;
+    [System.NonSerialized]
     public float frequency;
 
     public float spawnTime;
 
     private bool paused = false;
 
-	private float curX = 0;
+    private float curX = 0;
     private float baseFrequency = 0.015f;
-	private float vOffset = 1.9f;
-	private float barWidth = 0.4f;
+    private float vOffset = 1.9f;
+    private float barWidth = 0.4f;
 
     public float SinFunction(float x) {
         if (amplitude > cappedAmplitude) {
@@ -32,28 +34,28 @@ public class SineWaveSpawner : MonoBehaviour {
             spawnBar(0, 1, i);
             spawnBar(0, -1, i);
         }
-        
+
         StartCoroutine(SpawnLoop());
     }
 
     IEnumerator SpawnLoop() {
         while (!paused) {
             yield return new WaitForSeconds(spawnTime);
-			this.curX += this.baseFrequency;
+            this.curX += this.baseFrequency;
             float sin = this.SinFunction(this.curX);
-			spawnBar(sin, 1, 0);
-			spawnBar(sin, -1, 0);
+            spawnBar(sin, 1, 0);
+            spawnBar(sin, -1, 0);
         }
     }
 
-	void spawnBar(float sin,  int flip, int spawnPos) {
-		if (sin * flip < 0) {
-			sin = 0;  
-		}
-        GameObject bar = ObjectPooler.instance.GetPooledObjectAtPosition("WaveBar", new Vector3(this.transform.position.x + (spawnPos * barWidth), this.transform.position.y+(vOffset * flip), this.transform.position.z), Quaternion.identity);
-		if (bar != null) {
+    void spawnBar(float sin, int flip, int spawnPos) {
+        if (sin * flip < 0) {
+            sin = 0;
+        }
+        GameObject bar = ObjectPooler.instance.GetPooledObjectAtPosition("WaveBar", new Vector3(this.transform.position.x + (spawnPos * barWidth), this.transform.position.y + (vOffset * flip), this.transform.position.z), Quaternion.identity);
+        if (bar != null) {
             bar.GetComponent<WaveBar>().Init(numBars - spawnPos, spawnTime, barWidth).SetBarSprite(sin);
-			bar.gameObject.SetActive(true);
-		}
-	}
+            bar.gameObject.SetActive(true);
+        }
+    }
 }
