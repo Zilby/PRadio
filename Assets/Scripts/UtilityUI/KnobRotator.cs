5196ea5b118723752eba7c16fb012d5cb18b35ff
Alex Zilbersher <azilby@gmail.com>
2018-01-27 13:18:05 -0500

    made progress

---
+++ Assets/Scripts/UtilityUI/KnobRotator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KnobRotator : MonoBehaviour {
+
+	public Slider valueSlider;
+
+	private void Start()
+	{
+		valueSlider.onValueChanged.AddListener(SetRotation);
+	}
+
+	private void SetRotation(float value)
+	{
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, value * 180f - 90f));
+	}
+}
