4c186c0462f6153832c981bd2f65f05804c5dd9a
tday5 <trevday@icloud.com>
2018-01-26 20:24:04 -0500

    Initial sine commit

---
+++ Assets/Scripts/SinWave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinWave : MonoBehaviour {
+
+	// This is essentially cached, don't trust it
+	public float amplitude;
+
+	public float frequency;
+	
+	public float phase;
+
+	public float SinFunction(float xVal) {
+		return amplitude * Mathf.Sin(2 * Mathf.PI * frequency * xVal + phase);
+	}
+}
