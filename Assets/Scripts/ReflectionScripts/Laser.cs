276b96e015f5e51f6c80cedeb8feef21e082c7d5
tday5 <trevday@icloud.com>
2018-01-27 19:55:04 -0500

    initial reflection concept

---
+++ Assets/Scripts/ReflectionScripts/Laser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    public Vector2 momentum;
+
+    // Use this for initialization
+    void Awake() {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(momentum);
+    }
+
+    void OnBecameInvisible() {
+        Destroy(this.gameObject);
+    }
+}
