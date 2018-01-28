401e3b5173721130c86afe3ff66750de3c0c6452
Jonathan Paetzold <jcp.lucky@gmail.com>
2018-01-28 09:40:34 -0500

    Levels and fixes

--- Assets/Scripts/ReflectionScripts/LaserSpawner.cs
+++ Assets/Scripts/ReflectionScripts/LaserSpawner.cs
@@ -9,6 +9,18 @@ public class LaserSpawner : MonoBehaviour {
 
     public void Spawn() {
         instance = Instantiate(laserPrefab, gameObject.transform.position, Quaternion.identity);
+		if (this.transform.eulerAngles.z == 0) {
+			instance.setDirection(new Vector2 (200, 0));
+		}
+		if (this.transform.eulerAngles.z == 90) {
+			instance.setDirection(new Vector2 (0, 200));
+		}
+		if (this.transform.eulerAngles.z == 180) {
+			instance.setDirection(new Vector2 (-200, 0));
+		}
+		if (this.transform.eulerAngles.z == 270) {
+			instance.setDirection(new Vector2 (0, -200));
+		}
     }
 
     void Start() {
