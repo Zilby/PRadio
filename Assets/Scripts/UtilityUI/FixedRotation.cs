71e238d7ddfbf816b5dece7e4c03efc18d98201d
Alex Zilbersher <azilby@gmail.com>
2018-01-27 23:52:10 -0500

    holy shit these damn rainbows took 3 years

---
+++ Assets/Scripts/UtilityUI/FixedRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Don't look at me ;_;
+/// </summary>
+public class FixedRotation : MonoBehaviour {
+
+	Quaternion rotation;
+	Vector3 position;
+	Vector3 scale;
+	Transform parent;
+
+	void Awake()
+	{
+		parent = transform.parent;
+		transform.SetParent(transform.parent.parent.parent);
+		rotation = transform.rotation;
+		position = transform.position;
+		scale = transform.localScale;
+		transform.SetParent(parent);
+	}
+	void LateUpdate()
+	{
+		transform.SetParent(transform.parent.parent.parent);
+		transform.rotation = rotation;
+		transform.position = position;
+		transform.localScale = scale;
+		transform.SetParent(parent);
+	}
+}
--- Assets/Scripts/UtilityUI/KnobRotator.cs
+++ Assets/Scripts/UtilityUI/KnobRotator.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class KnobRotator : MonoBehaviour {
 
 	public Slider valueSlider;
+	public RectTransform mask;
 
 	private void Start()
 	{
@@ -13,5 +14,7 @@ public class KnobRotator : MonoBehaviour {
 	private void SetRotation(float value)
 	{
 		transform.rotation = Quaternion.Euler(new Vector3(0, 0, value * 180f));
+		float temp = 1.1f - (0.3f * value);
+		mask.pivot = new Vector2(mask.pivot.x, temp);
 	}
 }
