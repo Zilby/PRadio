683db528354177fc32f18155bb200937f75ea4a1
Alex Zilbersher <azilby@gmail.com>
2018-01-28 01:00:01 -0500

    improvements to pausing

--- Assets/Scripts/MainUI.cs
+++ Assets/Scripts/MainUI.cs
@@ -7,15 +7,25 @@ using UnityStandardAssets.ImageEffects;
 public class MainUI : MonoBehaviour {
 
 	public Button pauseButton;
+	public FadeableUI overlay;
 
 	// Use this for initialization
 	void Start () {
 		pauseButton.onClick.AddListener(Pause);
+		overlay.useUnscaledDeltaTimeForUI = true;
 	}
 
 	private void Pause()
 	{
 		GameManager.Pausing();
+		if(Time.timeScale == 0.0f)
+		{
+			overlay.SelfFadeIn();
+		}
+		else
+		{
+			overlay.SelfFadeOut();
+		}
 		Camera.main.GetComponent<BlurOptimized>().enabled = Time.timeScale == 0.0f;
 	}
 }
