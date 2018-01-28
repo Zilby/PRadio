4d7d6c489fffddc67399ef271cd081fc0641a441
tday5 <trevday@icloud.com>
2018-01-28 14:42:19 -0500

    maybe reflection works?

--- Assets/Scripts/ReflectionScripts/LaserSpawner.cs
+++ Assets/Scripts/ReflectionScripts/LaserSpawner.cs
@@ -7,26 +7,19 @@ public class LaserSpawner : MonoBehaviour {
     public Laser laserPrefab;
     private Laser instance;
 
+    void Start() {
+        instance = null;
+    }
+
     public void Spawn() {
         if (instance == null) {
             instance = Instantiate(laserPrefab, gameObject.transform.position, Quaternion.identity);
-            if (this.transform.eulerAngles.z == 0) {
-                instance.SetDirection(new Vector2(200, 0));
-            }
-            if (this.transform.eulerAngles.z == 90) {
-                instance.SetDirection(new Vector2(0, 200));
-            }
-            if (this.transform.eulerAngles.z == 180) {
-                instance.SetDirection(new Vector2(-200, 0));
-            }
-            if (this.transform.eulerAngles.z == 270) {
-                instance.SetDirection(new Vector2(0, -200));
-            }
+            instance.SetDirection(new Vector2(-200, 0));
         }
     }
 
     void Update() {
-        if (Input.GetKeyDown("l")) {
+        if (Input.GetKeyDown("space")) {
             Spawn();
         }
     }
